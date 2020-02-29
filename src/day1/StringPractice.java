ea3e9c2c482392ad1c66150da051bfa724a3919a
ranasahinler <ranaaral@hotmail.com>
2020-02-29 15:51:58 -0500

    update

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,5 +5,6 @@ public class StringPractice {
         System.out.println("Hello, world!");
         System.out.println("Jave is fun");
         System.out.println("bhuihkbg");
+        System.out.println("hvkjhvjv");
     }
 }
