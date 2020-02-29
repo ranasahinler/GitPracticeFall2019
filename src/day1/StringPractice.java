9c6b4a0f68a9d2b43a49f8856ac11074d6ed5273
ranasahinler <ranaaral@hotmail.com>
2020-02-29 15:43:25 -0500

    second commit

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -3,5 +3,6 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Jave is fun");
     }
 }
